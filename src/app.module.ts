8a968f8e47544d0eb896e881556f1f173c213ad7
Kisukeyas <kisuke.yas@gmail.com>
2022-08-18 22:40:18 +0900

    2022/8/18

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,23 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { User } from './users/user.entity';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: 'password',
+      database: 'nestUser',
+      entities: [User],
+      synchronize: true,
+    }),
+    UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
