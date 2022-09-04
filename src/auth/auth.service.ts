a30e14f771c5d2f8313cf7e477f6aabd9d99c68b
Kisukeyas <kisuke.yas@gmail.com>
2022-09-04 23:42:46 +0900

    2022/9/4

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,15 +1,20 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
 import { User } from 'src/users/user.entity';
 import { UsersService } from 'src/users/users.service';
+import * as bcrypt from "bcryptjs"
 
 
 @Injectable()
 export class AuthService {
     constructor(private readonly jwtService: JwtService, private readonly userService: UsersService){}
     async validateUser( { screenName, password }: User){
-        const user = this.userService.findOne(screenName);
-        return true;
+        const user = await this.userService.findOne(screenName);
+        const isValid = await bcrypt.compare( password, user['password']);
+        if (!isValid) {
+            throw new UnauthorizedException('Invalid credentials');
+        }
+        return isValid;
     }
     async login(user: User){
         if(await this.validateUser(user)){
